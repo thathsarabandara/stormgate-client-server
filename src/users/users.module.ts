7ee6e31923dd2a3e48f69b83e5c77c846da44825
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-11-18 16:09:07 +0530

    add user module

---
+++ src/users/users.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { UserPassword } from './entities/user-password.entity';
+import { Token } from './entities/token.entity';
+import { RefreshToken } from './entities/refresh-token.entity';
+import { PasswordReset } from './entities/password-reset.entity';
+import { UsersService } from './users.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      UserPassword,
+      Token,
+      RefreshToken,
+      PasswordReset
+    ])
+  ],
+  providers: [UsersService],
+  exports: [UsersService, TypeOrmModule],
+})
+export class UsersModule {}
